07845a5f232fb8272a894b0a599fd45a1b3222dd
cassandra-mn <arthenciac@gmail.com>
2022-03-10 23:41:18 -0300

    post dinâmico

--- src/Corpo.js
+++ src/Corpo.js
@@ -1,4 +1,4 @@
-const usuarios = [
+const stories = [
   { imagem: "assets/img/9gag.svg", nome: "9gag" },
   { imagem: "assets/img/meowed.svg", nome: "meowed" },
   { imagem: "assets/img/barked.svg", nome: "barked" },
@@ -9,91 +9,17 @@ const usuarios = [
   { imagem: "assets/img/memeriagourmet.svg", nome: "memeriagourmet" },
 ];
 
+const posts = [
+  { imagem: "assets/img/meowed.svg", nome: "meowed", perfil:"assets/img/respondeai.svg", conteudo: "assets/img/gato-telefone.svg", curtidas: "respondeai", quantidade: 101.523 },
+  { imagem: "assets/img/barked.svg", nome: "barked", perfil:"assets/img/adorable_animals.svg", conteudo: "assets/img/dog.svg", curtidas: "adorable_animals", quantidade: 99.159 }
+];
+
 export default function ConvolverOptions() {
   return (
     <div class="corpo">
       <div class="esquerda">
-        <div class="stories">
-          {usuarios.map(usuario => Stories(usuario))}
-
-          <div class="setinha">
-            <ion-icon name="chevron-forward-circle"></ion-icon>
-          </div>
-        </div>
-
-        <div class="posts">
-          <div class="post">
-            <div class="topo">
-              <div class="usuario">
-                <img src="assets/img/meowed.svg" />
-                meowed
-              </div>
-              <div class="acoes">
-                <ion-icon name="ellipsis-horizontal"></ion-icon>
-              </div>
-            </div>
-
-            <div class="conteudo">
-              <img src="assets/img/gato-telefone.svg" />
-            </div>
-
-            <div class="fundo">
-              <div class="acoes">
-                <div>
-                  <ion-icon name="heart-outline"></ion-icon>
-                  <ion-icon name="chatbubble-outline"></ion-icon>
-                  <ion-icon name="paper-plane-outline"></ion-icon>
-                </div>
-                <div>
-                  <ion-icon name="bookmark-outline"></ion-icon>
-                </div>
-              </div>
-
-              <div class="curtidas">
-                <img src="assets/img/respondeai.svg" />
-                <div class="texto">
-                  Curtido por <strong>respondeai</strong> e <strong>outras 101.523 pessoas</strong>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="post">
-            <div class="topo">
-              <div class="usuario">
-                <img src="assets/img/barked.svg" />
-                barked
-              </div>
-              <div class="acoes">
-                <ion-icon name="ellipsis-horizontal"></ion-icon>
-              </div>
-            </div>
-
-            <div class="conteudo">
-              <img src="assets/img/dog.svg" />
-            </div>
-
-            <div class="fundo">
-              <div class="acoes">
-                <div>
-                  <ion-icon name="heart-outline"></ion-icon>
-                  <ion-icon name="chatbubble-outline"></ion-icon>
-                  <ion-icon name="paper-plane-outline"></ion-icon>
-                </div>
-                <div>
-                  <ion-icon name="bookmark-outline"></ion-icon>
-                </div>
-              </div>
-
-              <div class="curtidas">
-                <img src="assets/img/adorable_animals.svg" />
-                <div class="texto">
-                  Curtido por <strong>adorable_animals</strong> e <strong>outras 99.159 pessoas</strong>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Stories />
+        <Posts />
       </div>
 
       <div class="sidebar">
@@ -184,15 +110,85 @@ export default function ConvolverOptions() {
   );
 }
 
-function Stories(usuario) {
+function Icon(props) {
+  return (
+    <ion-icon name={props.name}></ion-icon>
+  );
+}
+
+function Story(props) {
   return (
     <div class="story">
       <div class="imagem">
-        <img src={usuario.imagem} />
+        <img src={props.imagem} />
       </div>
       <div class="usuario">
-        {usuario.nome}
+        {props.nome}
       </div>
+    </div>
+  );
+}
+
+function Stories() {
+  return (
+    <div class="stories">
+      {stories.map(story =>
+        <Story imagem={story.imagem} nome={story.nome} />
+      )}
+
+      <div class="setinha">
+        <Icon name="chevron-forward-circle" />
+      </div>
+    </div>
+  );
+}
+
+function Post(props) {
+  return (
+    <div class="post">
+      <div class="topo">
+        <div class="usuario">
+          <img src={props.imagem} />
+          {props.nome}
+        </div>
+        <div class="acoes">
+          <Icon name="ellipsis-horizontal" />
+        </div>
+      </div>
+
+      <div class="conteudo">
+        <img src={props.conteudo} />
+      </div>
+
+      <div class="fundo">
+        <div class="acoes">
+          <div>
+            <Icon name="heart-outline" />
+            <Icon name="chatbubble-outline" />
+            <Icon name="paper-plane-outline" />
+          </div>
+          <div>
+            <Icon name="bookmark-outline" />
+          </div>
+        </div>
+
+        <div class="curtidas">
+          <img src={props.perfil} />
+          <div class="texto">
+            Curtido por <strong>{props.curtidas}</strong> e <strong>outras {props.quantidade} pessoas</strong>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Posts() {
+  return (
+    <div class="posts">
+      {posts.map(post =>
+        <Post imagem={post.imagem} nome={post.nome} conteudo={post.conteudo} perfil={post.perfil} curtidas={post.curtidas} quantidade={post.quantidade}/>
+      )}
     </div>
   );
 }
